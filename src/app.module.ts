850982ac0d4b7da111c7c38bfaade76590ed9967
gregodiaz <diazgregoriocruz@gmail.com>
2023-06-09 13:14:43 -0300

    + mocked user module for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
